4033a2bff4bf75aae2258b408881db1fc9dcb406
Mariia Konovalchuk <71904680+MariiaKon@users.noreply.github.com>
2022-03-19 00:24:01 +0200

    task 2

--- src/components/statistics/Statistics.styled.jsx
+++ src/components/statistics/Statistics.styled.jsx
@@ -5,6 +5,7 @@ import { setRandomHexColor } from 'components/statistics/setRandomColor';
 export const StatSection = styled.section`
   box-shadow: 0px 0px 3px rgb(80, 100, 200);
   margin: auto auto 70px auto;
+  width: 700px;
 `;
 
 export const Title = styled.h2`
